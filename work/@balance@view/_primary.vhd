4ac89f8b5b01f005d4c12762f95a5b257f76a41e
usefss <flasnm@yahoo.com>
2019-01-25 21:57:34 +0330

    Starting NEW Version

--- work/@balance@view/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity BalanceView is
-    port(
-        ID              : in     vl_logic_vector(3 downto 0);
-        ShowBalance     : in     vl_logic;
-        BalanceValue    : out    vl_logic_vector(4 downto 0)
-    );
-end BalanceView;
